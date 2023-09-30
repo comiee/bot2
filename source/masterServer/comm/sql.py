8a3d7556e4beb72482131f3402e0553743b87105
comiee <1440667228@qq.com>
2023-09-30 15:51:52 +0800

    增强sql处理逻辑

--- source/masterServer/comm/sql.py
+++ source/masterServer/comm/sql.py
@@ -1,9 +1,13 @@
+from public.log import public_logger
 import pymysql
 
 
 class _Sql:
     def __init__(self):
-        self.__db = pymysql.connect(
+        self.__db = self.__connect()
+
+    def __connect(self):
+        return pymysql.connect(
             host='localhost',
             user='comiee',
             password='19980722',
@@ -12,27 +16,40 @@ class _Sql:
         )
 
     def cursor(self):
+        if not self.__db.open:
+            self.__db = self.__connect()
         return self.__db.cursor()
 
     def execute(self, query):
-        with self.cursor() as cur:
-            return cur.execute(query)
+        try:
+            with self.cursor() as cur:
+                return cur.execute(query)
+        except pymysql.Error as e:
+            public_logger.error(f'sql执行execute出错：{e!r}')
 
     def query(self, query, default=None):
-        with self.cursor() as cur:
-            if cur.execute(query):
-                res = cur.fetchone()
-                if len(res) == 1:
-                    return res[0]
+        try:
+            with self.cursor() as cur:
+                if cur.execute(query):
+                    res = cur.fetchone()
+                    if len(res) == 1:
+                        return res[0]
+                    else:
+                        return res
                 else:
-                    return res
-            else:
-                return default
+                    return default
+        except pymysql.Error as e:
+            public_logger.error(f'sql执行query出错：{e!r}')
+            return default
 
     def query_all(self, query):
-        with self.cursor() as cur:
-            cur.execute(query)
-            return cur.fetchall()
+        try:
+            with self.cursor() as cur:
+                cur.execute(query)
+                return cur.fetchall()
+        except pymysql.Error as e:
+            public_logger.error(f'sql执行query_all出错：{e!r}')
+            return []
 
 
 sql = _Sql()
