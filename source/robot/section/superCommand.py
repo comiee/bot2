5da39eeb6ead464db314580276e599a5c321a057
comiee <1440667228@qq.com>
2024-04-14 19:24:18 +0800

    二选一类问题概率下调10% 增加生成刮刮乐功能 gain命令艾特不生效问题修复

--- source/robot/section/superCommand.py
+++ source/robot/section/superCommand.py
@@ -53,7 +53,7 @@ async def gain(session: Session, user: str, num: str):
 
 @SplitCommand('gain').trim_super()
 async def gain(session: Session, user_str: str, num_str: str, currency_str: str):
-    user = User(user_str)
+    user = User(int(''.join(c for c in user_str if c.isdigit())))
     num = int(num_str)
     currency = Currency[currency_str]
     old = user.query(currency)
@@ -61,20 +61,5 @@ async def gain(session: Session, user_str: str, num_str: str, currency_str: str)
     new = user.query(currency)
     await session.reply(f'执行成功，用户{user} {currency.value} {old}->{new}')
 
-
-@NormalCommand('gain').trim_super()
-async def gain_(session: Session, _):  # 函数名不能是gain，会破坏重载
-    message_list = list(session.event.message)  # 列表第一项为MessageSegment<Source>，计算长度要+1
-    if len(message_list) == 4 \
-            and message_list[1].type == 'Plain' and str(message_list[1]).strip() == 'gain' \
-            and message_list[2].type == 'At' \
-            and message_list[3].type == 'Plain':
-        args = str(message_list[3]).strip().split()
-        if len(args) == 1 or len(args) == 2:
-            num, currency, *_ = *args, 'coin'
-            # SplitCommand的重载是用多个不同的命令实现的，此处只能调用最后一次定义的函数
-            await gain(session, message_list[2].data['target'], num, currency)
-            return
-    session.skip()
 
 # TODO 保存自定义代码
