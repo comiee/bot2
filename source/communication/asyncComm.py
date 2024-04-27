f57b2c2a0226c8cc988335fc52fa173467f9a67b
comiee <1440667228@qq.com>
2024-04-27 17:04:25 +0800

    异步消息新增bytes类型 异步消息替换read为readexactly，防止数据过长时接受不全

--- source/communication/asyncComm.py
+++ source/communication/asyncComm.py
@@ -18,7 +18,9 @@ async def async_send_bytes(writer: StreamWriter, msg: bytes) -> None:
 
 
 async def async_send(writer: StreamWriter, obj) -> None:
-    if isinstance(obj, str):
+    if isinstance(obj, bytes):
+        await async_send_bytes(writer, b'bytes:' + obj)
+    elif isinstance(obj, str):
         await async_send_bytes(writer, b'str:' + obj.encode(ENCODING))
     elif isinstance(obj, dict):
         await async_send_bytes(writer, b'json:' + json.dumps(obj, ensure_ascii=False).encode(ENCODING))
@@ -27,18 +29,20 @@ async def async_send(writer: StreamWriter, obj) -> None:
 
 
 async def async_recv_bytes(reader: StreamReader) -> bytes:
-    s = (await reader.read(5)).decode(ENCODING)
+    s = (await reader.readexactly(5)).decode(ENCODING)
     if s == '':
         raise ConnectionError()
     n = int(s)
-    length = int((await reader.read(n)).decode(ENCODING))
-    return await reader.read(length)
+    length = int((await reader.readexactly(n)).decode(ENCODING))
+    return await reader.readexactly(length)
 
 
 async def async_recv(reader: StreamReader) -> str:
     msg = await async_recv_bytes(reader)
     type_, val = msg.split(b':', 1)
-    if type_ == b'str':
+    if type_ == b'bytes':
+        return val
+    elif type_ == b'str':
         return val.decode(ENCODING)
     elif type_ == b'json':
         return json.loads(val.decode(ENCODING))
