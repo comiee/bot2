8933d1e7a79741bfd0cca32644c0da757d2164e8
comiee <1440667228@qq.com>
2024-05-02 15:39:22 +0800

    momo日历优化：跳过全天类日程

--- testcase/test_masterServer/test_momo_calendar.py
+++ testcase/test_masterServer/test_momo_calendar.py
@@ -1,12 +1,21 @@
-from masterServer.module.momo_calendar import momo_calendar
+from masterServer.module.momo_calendar import CalendarBrowser
 import unittest
+import os
 
 
 class MomoCalendarTestCase(unittest.TestCase):
     def test_generate(self):
-        path = momo_calendar()
-        with open(path, encoding='utf-8') as f:
+        CalendarBrowser.generate()
+        self.assertFalse(CalendarBrowser.need_update())
+        with open(CalendarBrowser.ICS_PATH, encoding='utf-8') as f:
             self.assertNotEqual('', f.read())
+
+    def test_need_update(self):
+        if os.path.exists(CalendarBrowser.ICS_PATH):
+            os.remove(CalendarBrowser.ICS_PATH)
+        self.assertTrue(CalendarBrowser.need_update())
+        with getattr(CalendarBrowser, '_CalendarBrowser__update_lock'):
+            self.assertFalse(CalendarBrowser.need_update())
 
 
 if __name__ == '__main__':
