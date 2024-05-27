a8648dbdd0f07060f7a8fec166c00f65906e5c29
comiee <1440667228@qq.com>
2024-05-27 23:32:17 +0800

    inquiry界面问题修复

--- source/webpage/views/inquiry.py
+++ source/webpage/views/inquiry.py
@@ -3,11 +3,13 @@ from public.currency import Currency
 from webpage.webClient import get_web_client
 from webpage.mei.urls import register_url
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
 @register_url('inquiry')
+@csrf_exempt
 def inquiry(request):
-    if request.GET:
+    if request.GET or 'qq' not in request.POST:
         return render(request, 'inquiry.html')
     result = ''
     if qq := request.POST['qq']:
