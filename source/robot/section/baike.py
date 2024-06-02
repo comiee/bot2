34b122b9b2a41740393dde18eb263ea9dbb1bd6e
comiee <1440667228@qq.com>
2024-06-02 16:06:27 +0800

    百科功能问题为空场景防护

--- source/robot/section/baike.py
+++ source/robot/section/baike.py
@@ -4,9 +4,11 @@ from robot.comm.pluginBase import Session
 
 
 @SplitArgCommand('百科', ['请输入要查询的内容：'])
-@RegexCommand(r'^(?:什么|谁)是(.*)')
+@RegexCommand(r'^(?:什么|谁)是(.*)')  # TODO 这两种方式匹配到的概率过高，可能会干扰聊天
 @RegexCommand(r'(.*)是(?:什么|谁)$')
 async def baike(session: Session, question: str):
+    if not question.strip():
+        session.skip()
     async with AsyncClient('baike') as client:
         res = await client.send(question)
         res += '\n————来自百度百科'
