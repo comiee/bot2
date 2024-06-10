75dd49d69706005ae91a3b7782e67702de56e260
comiee <1440667228@qq.com>
2024-06-10 17:35:11 +0800

    理想状态为等待图片期间提前打开浏览器、接受请求时如果浏览器未关闭则复用之前的浏览器，由于太过复杂，可能有线程安全问题，且此功能一般不会有大量请求，暂且搁置

--- source/masterServer/module/identify_image.py
+++ source/masterServer/module/identify_image.py
@@ -1,16 +1,21 @@
 from communication.asyncServer import AsyncServer
 from selenium.webdriver.common.by import By
 from masterServer.comm.FirefoxBrowser import FirefoxBrowser
+from queue import Queue
 import asyncio
 
+_in_queue=Queue()
+_out_queue=Queue()
 
-def ascii2d(url):
+def ascii2d():
     with FirefoxBrowser() as browser:
-        browser.get('https://www.ascii2d.net/search/url/' + url)
-        items = browser.wait_elements(By.XPATH, '//*[@class="detail-box gray-link"]/h6/a[1]')
-        return [x.get_attribute('href') for x in items]
+        for url in _in_queue.get_nowait():
+            browser.get('https://www.ascii2d.net/search/url/' + url)
+            items = browser.wait_elements(By.XPATH, '//*[@class="detail-box gray-link"]/h6/a[1]')
+            _out_queue.put_nowait([x.get_attribute('href') for x in items])
 
 
 @AsyncServer().register('identify_image')
 async def identify_image(url):
-    return await asyncio.to_thread(ascii2d, url)
+    async with asyncio.Lock():
+        return await asyncio.to_thread(ascii2d, url)
--- source/robot/section/identify_image.py
+++ source/robot/section/identify_image.py
@@ -6,11 +6,11 @@ from robot.comm.command import SplitArgCommand
 
 @SplitArgCommand('识图', ['请输入图片'])
 async def identify_image(session: Session, pic_str):
-    try:
-        url = get_pic_url_from_internal(pic_str)
-    except AssertionError:
-        await session.reply('未找到图片')
-        return
-    async with AsyncClient('identify_image') as client:
+    async with AsyncClient('identify_image') as client:  # 提前发送cmd过去，让服务器提前打开浏览器
+        try:
+            url = get_pic_url_from_internal(pic_str)
+        except AssertionError:
+            await session.reply('未找到图片')
+            return
         res = await client.send(url)
         await session.reply('找到以下内容：\n' + '\n'.join(res[:3]))
