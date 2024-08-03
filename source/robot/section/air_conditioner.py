9760ece99ca522a93d11940815de3199791317a9
comiee <1440667228@qq.com>
2024-08-03 19:47:09 +0800

    空调控制在robot端增加校验，命令已被学习时不发送“开始学习”

--- source/robot/section/air_conditioner.py
+++ source/robot/section/air_conditioner.py
@@ -6,6 +6,11 @@ from robot.comm.command import NormalCommand, FullCommand
 @NormalCommand('空调学习').trim_super().trim_friend()
 async def air_conditioner_learn(session: Session, cmd):
     cmd = cmd.strip()
+    async with AsyncClient('air_conditioner_get_all') as client:
+        cmds = await client.send('')
+        if cmd in cmds:
+            await session.reply(f'已有同名命令，当前已学习的命令：{cmds}')
+            return
     async with AsyncClient('air_conditioner_learn') as client:
         await session.reply('开始学习')
         res = await client.send(cmd)
