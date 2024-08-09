f281ac596f0ee9d608b8e35f69b5bbdcd2e19922
comiee <1440667228@qq.com>
2024-08-09 20:37:28 +0800

    qqbot添加私聊回复

--- source/qqbot/main.py
+++ source/qqbot/main.py
@@ -2,7 +2,7 @@ from public.config import get_config
 from public.log import qq_bot_logger
 from qqbot.comm.Command import SingleCommand
 import botpy
-from botpy.message import GroupMessage
+from botpy.message import GroupMessage, C2CMessage
 from threading import Thread
 import asyncio
 
@@ -11,8 +11,8 @@ class MyClient(botpy.Client):
     async def on_ready(self):
         qq_bot_logger.info(f"qqbot 「{self.robot.name}」 on_ready!")
 
-    async def on_group_at_message_create(self, message: GroupMessage):
-        content=message.content.strip()
+    async def handle_message(self, message):
+        content = message.content.strip()
         if content.startswith('/'):
             cmd, text, *_ = *content[1:].split(maxsplit=1), ''
             # TODO 当前仅考虑单次命令，后续添加其他命令的适配
@@ -20,6 +20,12 @@ class MyClient(botpy.Client):
                 await command.handle(message, text)
                 return
         # TODO chat
+
+    async def on_group_at_message_create(self, message: GroupMessage):
+        await self.handle_message(message)
+
+    async def on_c2c_message_create(self, message: C2CMessage):
+        await self.handle_message(message)
 
 
 def run_qqbot():
