0b170ba2c31d85ac412a447a1695f63a896a13c1
comiee <1440667228@qq.com>
2024-10-06 17:30:25 +0800

    修复ai命令文本中有空格时会被分割成多个参数导致无法匹配的问题

--- source/robot/section/ai_chat.py
+++ source/robot/section/ai_chat.py
@@ -1,11 +1,11 @@
 from communication.asyncClient import AsyncClient
 from public.convert import convert_to
-from robot.comm.command import SplitCommand, NormalCommand
+from robot.comm.command import NormalCommand
 from robot.comm.pluginBase import Session
 
 
-@SplitCommand('ai')
-@SplitCommand('AI')
+@NormalCommand('ai ')
+@NormalCommand('AI ')
 async def ai_chat(session: Session, text: str):
     async with AsyncClient('ai_chat') as client:
         ret = await client.send(convert_to('plain', text))
