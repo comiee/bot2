f5581d7dfa53518c859205360d6592ac322e4871
comiee <1440667228@qq.com>
2025-02-22 20:24:25 +0800

    每个进程使用单独的日志文件，防止多进程同时访问同一个文件冲突

--- source/main.py
+++ source/main.py
@@ -6,13 +6,13 @@ import multiprocessing
 import time
 
 
-def start_main(main_fun):
-    multiprocessing.Process(target=main_fun).start()
+def start_main(main_fun, name=None):
+    multiprocessing.Process(target=main_fun, name=name).start()
 
 
 def main():
-    start_main(bot_main)
-    start_main(web_main)
+    start_main(bot_main, 'bot')
+    start_main(web_main, 'web')
 
     time.sleep(1)  # pycharm的bug，不加sleep会卡住
     server_main(CmdAdmin)  # CmdAdmin里面有input，只能在主线程运行
--- source/public/log.py
+++ source/public/log.py
@@ -1,8 +1,9 @@
-from comiee import overload
+from comiee import overload, lazy
 from public.config import data_path
 from enum import IntEnum
 import logging
 from logging.handlers import RotatingFileHandler
+from multiprocessing import current_process
 
 LOG_PATH = data_path('log')
 
@@ -39,31 +40,32 @@ def get_logger(name, level, file_name, file_level) -> logging.Logger:
     ch.setFormatter(formatter)
     logger.addHandler(ch)
 
-    fh = RotatingFileHandler(str(data_path('log', file_name)), encoding='utf-8',
-                             maxBytes=1024 * 1024, backupCount=5)
+    a = file_name.split('.')
+    name = f'{".".join(a[:-1])}_{current_process().name}.{a[-1]}'
+    fh = RotatingFileHandler(str(data_path('log', name)),
+                             encoding='utf-8', maxBytes=1024 * 1024, backupCount=5)
     fh.setLevel(file_level)
     fh.setFormatter(formatter)
     logger.addHandler(fh)
     return logger
 
 
-# TODO 多进程使用同一个logger对象，会导致抢占日志文件
 # 底层的client和server使用logger
-client_logger: logging.Logger = get_logger('client', LogLevel.INFO, LogLevel.DEBUG)
-server_logger: logging.Logger = get_logger('server', LogLevel.DEBUG)
-async_client_logger: logging.Logger = get_logger('asyncClient', LogLevel.DEBUG)
-async_server_logger: logging.Logger = get_logger('asyncServer', LogLevel.INFO, LogLevel.DEBUG)
+client_logger: logging.Logger = lazy(get_logger, 'client', LogLevel.INFO, LogLevel.DEBUG)
+server_logger: logging.Logger = lazy(get_logger, 'server', LogLevel.DEBUG)
+async_client_logger: logging.Logger = lazy(get_logger, 'asyncClient', LogLevel.DEBUG)
+async_server_logger: logging.Logger = lazy(get_logger, 'asyncServer', LogLevel.INFO, LogLevel.DEBUG)
 
 # 公共部分使用的logger
-public_logger: logging.Logger = get_logger('public', LogLevel.DEBUG)
+public_logger: logging.Logger = lazy(get_logger, 'public', LogLevel.DEBUG)
 
 # 服务器各组件使用的logger
-master_server_logger: logging.Logger = get_logger('masterServer', LogLevel.INFO, LogLevel.DEBUG)
-admin_logger: logging.Logger = get_logger('admin', LogLevel.DEBUG)
+master_server_logger: logging.Logger = lazy(get_logger, 'masterServer', LogLevel.INFO, LogLevel.DEBUG)
+admin_logger: logging.Logger = lazy(get_logger, 'admin', LogLevel.DEBUG)
 
 # bot客户端各组件使用的logger
-bot_client_logger: logging.Logger = get_logger('botClient', LogLevel.DEBUG)
-bot_logger: logging.Logger = get_logger('bot', LogLevel.DEBUG)
+bot_client_logger: logging.Logger = lazy(get_logger, 'botClient', LogLevel.DEBUG)
+bot_logger: logging.Logger = lazy(get_logger, 'bot', LogLevel.DEBUG)
 
 # web客户端各组件使用的logger
-web_logger: logging.Logger = get_logger('webClient', LogLevel.DEBUG)
+web_logger: logging.Logger = lazy(get_logger, 'webClient', LogLevel.DEBUG)
